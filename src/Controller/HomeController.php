9e3b007f7a9db32e4188377167cb7ec3cf76a892
Rachel MICHEL <michel.rachel@hotmail.fr>
2021-05-13 09:44:21 +0200

    Change login page to homepage + add page about us + terms of use

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -11,10 +11,26 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function home(): Response
     {
-        return $this->render('home.html.twig', [
+        return $this->render('home/home.html.twig', [
             'controller_name' => 'HomeController',
         ]);
+    }
+
+	/**
+     * @Route("/about-us", name="about_us")
+     */
+    public function aboutUs(): Response
+    {
+        return $this->render('home/about_us.html.twig');
+    }
+
+	/**
+     * @Route("/terms-of-use-and-privacy-policy", name="terms_of_use")
+     */
+    public function termsOfUse(): Response
+    {
+        return $this->render('home/terms_of_use.html.twig');
     }
 }
