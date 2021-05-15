955fddcfdd7418ea9a806d61582311dbb8acc3a6
Rachel MICHEL <michel.rachel@hotmail.fr>
2021-05-15 15:17:28 +0200

    Page manage user for admin

---
+++ src/Controller/Admin/ManageUserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\User;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+/**
+ * @Route("/admin", name="admin_")
+ */
+class ManageUserController extends AbstractController
+{
+    /**
+     * @Route("/users", name="users")
+     */
+    public function users(): Response
+    {
+        return $this->render('admin/user_list.html.twig', [
+            'users' => $this->getDoctrine()->getRepository(User::class)->findByRole(User::ROLE_USER),
+            'raidLeaders' => $this->getDoctrine()->getRepository(User::class)->findByRole(User::ROLE_RAID_LEADER),
+        ]);
+    }
+
+	/**
+     * @Route("/promote-or-demote/user/{id}", name="promote_or_demote")
+     */
+    public function promoteOrDemote(User $user): Response
+    {
+		if ($user->getStrRole() === User::ROLE_USER) {
+			$user->setRoles([User::ROLE_RAID_LEADER]);
+		} else if ($user->getStrRole() === User::ROLE_RAID_LEADER) {
+			$user->setRoles([User::ROLE_USER]);
+		}
+
+		$this->getDoctrine()->getManager()->flush();
+
+        return $this->redirectToRoute('admin_users');
+    }
+
+}
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -36,32 +36,16 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
         $this->_em->flush();
     }
 
-    // /**
-    //  * @return User[] Returns an array of User objects
-    //  */
-    /*
-    public function findByExampleField($value)
+    /**
+     * @return User[] Returns an array of User objects
+     */
+    public function findByRole($role)
     {
         return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('u.id', 'ASC')
-            ->setMaxResults(10)
+            ->where('u.roles LIKE :role')
+            ->setParameter('role', "%".$role."%")
+            ->orderBy('u.name', 'ASC')
             ->getQuery()
-            ->getResult()
-        ;
+            ->getResult();
     }
-    */
-
-    /*
-    public function findOneBySomeField($value): ?User
-    {
-        return $this->createQueryBuilder('u')
-            ->andWhere('u.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
 }
