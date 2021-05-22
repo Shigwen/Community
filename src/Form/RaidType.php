eb53e64b1eac89582584ee991aad579d86f78945
Michael Donati <michael.donati@hotmail.com>
2021-05-22 15:10:29 +0200

    WIP

--- src/Form/RaidType.php
+++ src/Form/RaidType.php
@@ -10,6 +10,7 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 
 class RaidType extends AbstractType
 {
@@ -32,8 +33,7 @@ class RaidType extends AbstractType
 					'40' => 40,
 				],
 
-				'choice_attr' => function($raidType){
-					// return ['class' => 'fixed-bottom invisible disabled'];
+				'choice_attr' => function(){
 					return ['class' => 'btn-check'];
 				},
 
@@ -46,25 +46,110 @@ class RaidType extends AbstractType
 				'multiple' => false,
 			])
 
-            ->add('startAt', DateTimeType::class, [
-				'widget' => 'single_text'
-			])
-            ->add('endAt', DateTimeType::class, [
-				'widget' => 'single_text'
-			])
-            ->add('information')
-			->add('expectedAttendee')
-            ->add('minTank')
-            ->add('maxTank')
-            ->add('minHeal')
-            ->add('maxHeal')
 			->add('raidCharacter', RaidCharacterType::class, [
 				'user' => $options['user'],
 				'label' => false,
 				'mapped' => false,
 			])
-            ->add('autoAccept')
-			->add('isPrivate')
+
+            ->add('startAt', DateTimeType::class, [
+				'widget' => 'single_text',
+				'label' => 'Raid starts at :',
+				'label_attr' => [
+					'class' => 'h5',
+				],
+			])
+
+            ->add('endAt', DateTimeType::class, [
+				'widget' => 'single_text',
+				'label' => 'Raid ends at :',
+				'label_attr' => [
+					'class' => 'h5',
+				],
+			])
+
+			->add('expectedAttendee', null, [
+				'label' => 'Amount of raiders you\'re looking for',
+				'label_attr' => [
+					'class' => 'h5',
+				],
+				'attr' => [
+					'class' => 'col-2 form-control',
+				],
+			])
+
+            ->add('minTank', null, [
+				'label' => 'Min. number of Tanks (default : 1)',
+				'label_attr' => [
+					'class' => 'h5',
+				],
+				'attr' => [
+					'class' => 'col-2 form-control',
+				],
+			])
+
+            ->add('maxTank', null, [
+				'label' => 'Max. number of Tanks',
+				'label_attr' => [
+					'class' => 'h5',
+				],
+				'attr' => [
+					'class' => 'col-2 form-control',
+				],
+			])
+
+            ->add('minHeal', null, [
+				'label' => 'Min. number of Healers (default : 1)',
+				'label_attr' => [
+					'class' => 'h5',
+				],
+				'attr' => [
+					'class' => 'col-2 form-control',
+				],
+			])
+
+            ->add('maxHeal', null, [
+				'label' => 'Max. number of Healers',
+				'label_attr' => [
+					'class' => 'h5',
+				],
+				'attr' => [
+					'class' => 'col-2 form-control',
+				],
+			])
+
+            ->add('autoAccept', null, [
+				'label' => 'Auto-accept',
+				'label_attr' => [
+					'class' => 'form-check-label h5',
+				],
+				'attr' => [
+					'class' => 'form-check-input',
+				],
+			])
+
+			->add('isPrivate', null, [
+				'label' => 'Private raid',
+				'label_attr' => [
+					'class' => 'form-check-label h5',
+				],
+				'attr' => [
+					'class' => 'form-check-input',
+				],
+			])
+
+            ->add('information', TextareaType::class, [
+				'label' => 'Raid Leader notes',
+				'label_attr' => [
+					'class' => 'h5',
+				],
+				'attr' => [
+					'class' => 'form-control',
+					'rows' => '14',
+				],
+				'data' => 'truc',
+			])
+
 			->add('save', SubmitType::class, [
 				'label' => $options['isEdit'] ? 'Modify raid' : 'Create raid',
 			]);
@@ -73,9 +158,20 @@ class RaidType extends AbstractType
 				$builder
 					->add('templateName', TextType::class, [
 						'mapped' => false,
+						'label' => 'Give it a template name :',
+						'label_attr' => [
+							'class' => 'h5',
+						],
+						'attr' => [
+							'class' => 'form-control',
+							'placeholder' => 'ex: Taverns of Time - Wednesday Pug - Karazhan',
+						],
 					])
 					->add('saveTemplate', SubmitType::class, [
-						'label' => 'Save template'
+						'label' => 'Save template',
+						'attr' => [
+							'class' => 'btn btn-lg btn-primary',
+						],
 					]);
 			}
 
@@ -86,7 +182,10 @@ class RaidType extends AbstractType
 						'data' => $options['raidTemplate']->getName(),
 					])
 					->add('editTemplate', SubmitType::class, [
-						'label' => 'Edit template'
+						'label' => 'Edit template',
+						'attr' => [
+							'class' => 'btn btn-lg btn-primary',
+						],
 					]);
 			}
     }
