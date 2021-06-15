da17d3e7bf21ebb9698f6a0bec80b80afc48a286
Rachel MICHEL <michel.rachel@hotmail.fr>
2021-06-15 16:18:15 +0200

    correction

--- src/Twig/RaidListButtonExtension.php
+++ src/Twig/RaidListButtonExtension.php
@@ -21,14 +21,14 @@ class RaidListButtonExtension extends AbstractExtension
     {
         $buttons = [];
 
-        // Generates an array of all buttons not sorted
+        // Generates an array of all the not sorted buttons
         for ($i = 0; $i <= $nbrOfPages; $i++) {
             $class = ($i === $currentPage) ? 'btn-info current' : 'btn-primary';
             $pageNumber = $i + 1;
             $buttons[$i] = '<button data-page="' . $i . '" class="btn btn-sm rounded-pill mx-1 page ' . $class . '"> ' . $pageNumber . ' </button>';
         }
 
-        // If there is less than 9 page, we do not paginate
+        // If there is less than 9 pages, we do not paginate
         if ($nbrOfPages < 8) {
             return $buttons;
         }
@@ -45,7 +45,7 @@ class RaidListButtonExtension extends AbstractExtension
             $buttonsShow[] = '...';
             $buttonsShow = array_merge($buttonsShow, array_slice($buttons, -6, 6));
 
-        // Show the 5 middle buttons (2 before current, the current, 2 after current) and truncates the start and end buttons (except THE first and THE last)
+        // Show the 5 middle buttons (2 before current, the current, 2 after current) and truncate the start and end buttons (except THE first and THE last)
         } else {
             $cursor = $currentPage - 2;
 
