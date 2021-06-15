dccb750edc789f1ccae550f2c4b5ac68eb25acf7
Rachel MICHEL <michel.rachel@hotmail.fr>
2021-06-15 23:34:44 +0200

    array minutes

--- src/Form/RaidType.php
+++ src/Form/RaidType.php
@@ -56,6 +56,7 @@ class RaidType extends AbstractType
 
             ->add('startAt', DateTimeType::class, [
                 'widget' => 'choice',
+                'minutes' => [0, 15, 30, 45],
                 'label' => 'Raid starts at :',
                 'label_attr' => [
                     'class' => 'h5',
@@ -65,6 +66,7 @@ class RaidType extends AbstractType
 
             ->add('endAt', DateTimeType::class, [
                 'widget' => 'choice',
+                'minutes' => [0, 15, 30, 45],
                 'label' => 'Raid ends at :',
                 'label_attr' => [
                     'class' => 'h5',
